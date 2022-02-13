4f4a0b8a537b084d04f4dfd660d49a225978e944
Morgan Blackmore <40365252+morgandblackmore@users.noreply.github.com>
2022-02-12 23:58:18 -0800

    minor changes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,9 +7,9 @@ class TestRoutes(flask_unittest.ClientTestCase):
     def test_index(self, client):
         self.assertStatus(client.get("/"), 200)
 
-    def test_add_link(self, client, app):
-        response = client.post('/add_link', data={'original_url': 'https://www.google.com'}, follow_redirects=True)
-        
+    # def test_add_link(self, client, app):
+        # response = client.post('/add_link', data={'original_url': 'https://www.google.com'}, follow_redirects=True)
+
 
     def test_errorhandler(self, client):
         self.assertStatus(client.get("/404"), 404)
