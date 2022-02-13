fd1557c7da3ec5c4bd9fb79ff0f1aacc5db5c16f
Morgan Blackmore <40365252+morgandblackmore@users.noreply.github.com>
2022-02-12 22:33:51 -0800

    env files added to facilitate local hosting

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,15 +7,9 @@ class TestRoutes(flask_unittest.ClientTestCase):
     def test_index(self, client):
         self.assertStatus(client.get("/"), 200)
 
-    def test_add_link(self, client):
-        # self.assertStatus(client.get("/add_link"), 200)
-
+    def test_add_link(self, client, app):
         response = client.post('/add_link', data={'original_url': 'https://www.google.com'}, follow_redirects=True)
-        print('morgan')
-        # with app.app_context():
-        #     self.assertIsNotNone(
-        #         get_db().execute("select * from Link where original_url = 'https://www.google.com'").fetchone()
-        #     )
+        
 
     def test_errorhandler(self, client):
         self.assertStatus(client.get("/404"), 404)
